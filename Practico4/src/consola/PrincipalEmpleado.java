6729fadd5c1a9416c8604aa020a044afe7857fa3
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2023-08-24 17:59:00 -0300

    Update PrincipalEmpleado.java

--- Practico4/src/consola/PrincipalEmpleado.java
+++ Practico4/src/consola/PrincipalEmpleado.java
@@ -3,10 +3,7 @@ import logica.*;
 public class PrincipalEmpleado {
 
 	public static void main(String[] args) {
-		Fecha f1=new Fecha(12,3,2022);
-		Empleado e1=new Empleado("222","Juan",20000,f1);
-    System.out.println(e1);
-
+		
 	}
 
 }
