e24a78d11763cbdd9b855b63ae31694367afd82e
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2023-08-24 17:58:43 -0300

    Update PrincipalBecado.java

--- Practico4/src/consola/PrincipalBecado.java
+++ Practico4/src/consola/PrincipalBecado.java
@@ -3,20 +3,7 @@ import logica.*;
 public class PrincipalBecado {
 
 	public static void main(String[] args) {
-		Fecha f= new Fecha(18,1,2000);
-		Becado b1 = new Becado(100,"Perez", 1, 1000, f);
-		Becado b2 = new Becado();
-		b2.setApellido("Ruiz");
-		b2.setGrupo(2);
-		b2.setCuotaBase(2000);
-		b2.setNacimiento(f);
-		b2.setSaldoBeca(3000);
 		
-		Alumno a1 = new Alumno(b2.getApellido(), b2.getGrupo(), b2.getCuotaBase(), b2.getNacimiento());
-    System.out.println("a1 es: "+a1.toString());
-		System.out.println("la cuota neta de a1 es: "+a1.cuotaNeta());
-		System.out.println("b2 es: "+b2.toString());
-    System.out.println("la cuota de b2 es: "+b2.cuotaNeta());
 	}
 
 }
