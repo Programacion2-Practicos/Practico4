488b7ce237b7d647c7b6ce20ada778a4abef1d00
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2023-08-24 18:03:09 -0300

    Update Nacional.java

--- Practico4/src/logica/Nacional.java
+++ Practico4/src/logica/Nacional.java
@@ -1,47 +1,7 @@
 package logica;
 
-public class Nacional extends Articulo {
-	private String departamento;
-	private boolean subsidiado;
+public class Nacional  {
 	
-	//constructores
-	public Nacional() {
-		super();
-	}
-	public Nacional(String nombre , int codigo , double precioCosto , Fecha vencimiento, String departamento, boolean subsidiado) {
-		super( nombre,codigo,precioCosto, vencimiento);
-		this.departamento = departamento;
-		this.subsidiado = subsidiado;
-	}
-	//Setters y Getters
-	public String getDepartamento() {
-		return departamento;
-	}
-	public void setDepartamento(String departamento) {
-		this.departamento = departamento;
-	}
-	public boolean getSubsidiado() {
-		return subsidiado;
-	}
-	public void setSubsidiado(boolean subsidiado) {
-		this.subsidiado = subsidiado;
-	}
-	@Override
-	public double precioVenta() {
-		if (subsidiado==true) {
-			return super.precioVenta();
-		}
-		else 
-			if (subsidiado==false && (departamento.equals("Montevideo") || departamento.equals("montevideo"))) {
-				return (super.precioVenta()*0.15+super.precioVenta());
-			}
-			else
-				return (super.precioVenta()*0.10+super.precioVenta());
-	}
 	
-	@Override
-	public String toString() {
-		return super.toString()+" departamento=" + departamento + ",\n subsidiado=" + subsidiado +",\n el precio de venta= "+precioVenta();
-	}
 	
 }
